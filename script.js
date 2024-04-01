6cfb916fb83c194a907426a1da76cc1c4e6210d3
Pranay Dalvi <120023286+Pranaydalvi@users.noreply.github.com>
2024-04-01 14:25:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,15 @@ function calculateAge() {
     var age = currentDate.getFullYear() - birthdate.getFullYear();
     var months = currentDate.getMonth() - birthdate.getMonth();
 
-    // Handle negative months and adjust for days
     if (months < 0 || (months === 0 && currentDate.getDate() < birthdate.getDate())) {
         age--;
         months += 12;
     }
 
-    // Adjust for days when birth date day is greater than current date day
     if (currentDate.getDate() < birthdate.getDate()) {
         var lastMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, currentDate.getDate());
         var daysInLastMonth = (currentDate - lastMonth) / (1000 * 60 * 60 * 24);
-        months += daysInLastMonth / 30; // Assuming an average month length of 30 days
+        months += daysInLastMonth / 30;
     }
 
     document.getElementById("result").innerText = "Your age is: " + age + " years and " + Math.round(months) + " months";
